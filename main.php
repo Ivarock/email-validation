f6355d5202da81008a5810cd5085379389fff026
Ivarock <ivarssluka@gmail.com>
2024-05-21 10:55:24 +0300

    Initial commit

---
+++ main.php
@@ -0,0 +1,36 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Emailvalidation\EmailvalidationClient;
+use Dotenv\Dotenv;
+use SendGrid\Mail\Mail;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$apiKey = $_ENV['EMAIL_VALIDATION_API_KEY'];
+$emailvalidation = new EmailvalidationClient($apiKey);
+$userEmail = readline("Please enter your email: ");
+$response = $emailvalidation->validate($userEmail);
+
+if ($response['state'] === 'deliverable') {
+    echo "The email address is valid and deliverable.\n";
+
+    $email = new Mail();
+    $email->setFrom($_ENV['VERIFIED_SENDER'], "Email Validator");
+    $email->setSubject("Email Test");
+    $email->addTo($userEmail);
+    $email->addContent("text/plain", "Hello world!");
+
+    $sendgrid = new \SendGrid($_ENV['SENDGRID_API_KEY']);
+
+    try {
+        $response = $sendgrid->send($email);
+        echo 'Message has been sent successfully. Status code: ' . $response->statusCode();
+    } catch (Exception $exception) {
+        echo "Message could not be sent. Error: {$exception->getMessage()}";
+    }
+} else {
+    echo "The email address is not deliverable.\n";
+}
